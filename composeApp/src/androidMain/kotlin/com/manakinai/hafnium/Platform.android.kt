788802a6be5c677ba7b453a4ee0e50ea71f838d8
Devarshi Kulshreshtha <devarshi.kulshreshtha@flueid.com>
2025-11-11 17:17:05 +0530

    Initial commit from Specify template

---
+++ composeApp/src/androidMain/kotlin/com/manakinai/hafnium/Platform.android.kt
@@ -0,0 +1,9 @@
+package com.manakinai.hafnium
+
+import android.os.Build
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
---
+++ composeApp/src/iosMain/kotlin/com/manakinai/hafnium/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.manakinai.hafnium
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
